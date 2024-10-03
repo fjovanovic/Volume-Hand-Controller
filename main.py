c46a416301c391b6e90c166fda3eabb5344bd5e2
fjovanovic <filipjovanovic98@gmail.com>
2024-10-03 21:54:50 +0200

    Added FPS on the cam

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import sys
+import time
 
 from termcolor import colored
 import cv2
 
+import utils
+
 
 def main() -> None:
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    if cap.isOpened() == False:
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    if cam.isOpened() == False:
         sys.exit(colored(
             'Camera could not be opened',
             'red'
         ))
+    
+    previous_time = time.time()
 
     while True:
-        (success, img) = cap.read()
+        (success, img) = cam.read()
+        if success == False:
+            utils.error('Unable to process image', cam)
+        
+        # Measure the FPS
+        current_time = time.time()
+        fps = int(1 / (current_time - previous_time))
+        previous_time = current_time
+
+        cv2.putText(
+            img=img, 
+            text=f'FPS: {fps}', 
+            org=(10, 30), 
+            fontFace=cv2.FONT_HERSHEY_COMPLEX, 
+            fontScale=1, 
+            color=(0, 255, 0), 
+            thickness=1
+        )
 
         cv2.imshow('Volume hand controller', img)
         key = cv2.waitKey(1)
@@ -22,7 +44,7 @@ def main() -> None:
             break
 
     cv2.destroyAllWindows()
-    cap.release()
+    cam.release()
 
 
 if __name__ == '__main__':
